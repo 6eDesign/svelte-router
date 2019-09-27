31d7871a60471992604997107f4346485bd5a960
Jonathan Greenemeier <jon@6eDesign.net>
2019-09-27 17:56:07 -0600

    fix: main.js export

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Router from './Components/Router.html';
-import Route from './Components/Route.html';
+import Router from './Components/Router.svelte';
+import Route from './Components/Route.svelte';
 
 export { Router, Route }; 
