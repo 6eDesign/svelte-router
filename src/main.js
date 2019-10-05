c946a53769b6811e9044847acdc5ae67ddc134f8
Jonathan Greenemeier <jon@6eDesign.net>
2019-10-04 23:03:11 -0600

    fix: routing issue

--- src/main.js
+++ src/main.js
@@ -4,14 +4,7 @@ import { ROUTER } from './Components/Router.svelte';
 import Route from './Components/Route.svelte';
 
 
-const getRouterCtx = () => {
-  const ctx = getContext(ROUTER)
-  ctx.selectedRoute.update(selected => {
-    delete selected.ctx.page;
-    return selected;
-  });
-  return ctx;
-};
+const getRouterCtx = () => getContext(ROUTER);
 
 export { 
   getRouterCtx as getContext,
